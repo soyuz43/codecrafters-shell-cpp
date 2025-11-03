e6227561df7b8ef1e1162f5c355f094364de970f
soyuz43 <kebekad673@proton.me>
2025-11-03 17:00:36 -0600

    add REPL loop

--- src/main.cpp
+++ src/main.cpp
@@ -2,12 +2,18 @@
 #include <string>
 
 int main() {
-  // Flush after every std::cout / std:cerr
+  // Flush after every std::cout / std::cerr
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
 
-  std::cout << "$ ";
-  std::string command;
-  std::getline(std::cin, command);
-  std::cerr << command << ": command not found" << std::endl;
+  while (true) {
+    std::cout << "$ ";
+    std::string command;
+    std::getline(std::cin, command);
+    if (!std::cin.eof()) {
+      std::cerr << command << ": command not found" << std::endl;
+    } else {
+      break;
+    }
+  }
 }
